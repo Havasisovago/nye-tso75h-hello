d283f312cbd4ff4c6ca94ae698955ccfae3b35d8
Kovacs Gabor <kovacs0gabor1999@gmail.com>
2022-09-25 23:40:02 +0200

    hello-jar-v2

--- src/main/java/hu/nye/progtech/hello/Main.java
+++ src/main/java/hu/nye/progtech/hello/Main.java
@@ -1,7 +1,29 @@
 package hu.nye.progtech.hello;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("hello-jar");
+        Scanner input = new Scanner(System.in);
+        String name;
+        String lang;
+        System.out.print("Add meg a kereszt neved:");
+        name = input.nextLine();
+        System.out.print("Add meg a nyelved:");
+        lang = input.nextLine();
+
+
+        switch(lang) {
+            case "hun":
+                System.out.println(" Szia" + name + "!" );
+                break;
+            case "eng":
+                System.out.println(" Hello" + name + "!" );
+            case "haw":
+                System.out.println(" Aloha" + name + "!" );
+                break;
+            default:
+                System.out.println(" Szia" + name + "!" );
+        }
     }
 }
